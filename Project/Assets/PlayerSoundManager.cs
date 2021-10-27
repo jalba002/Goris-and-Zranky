5c9d763d43551b667eca0f4cf34da5ed06bdeb77
Andrew <73665985+andrewraya@users.noreply.github.com>
2021-10-27 17:08:50 +0200

    soundmangaer

---
+++ Project/Assets/PlayerSoundManager.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerSoundManager : MonoBehaviour
+{
+    public AudioSource audioSource;
+
+    public void SoundPlayer(Object clip)
+    {
+        audioSource.clip = clip as AudioClip;
+        audioSource.Play();
+        Debug.Log("Playing Jump");
+    }
+}
