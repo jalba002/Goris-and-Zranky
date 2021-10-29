ed92a6d78237813338e623361b45e52445ae402e
Jordi Alba <44704875+jalba002@users.noreply.github.com>
2021-10-29 19:14:42 +0200

    Interact miniupdate

--- Project/Assets/Scripts/Game Elements/InteractableObject.cs
+++ Project/Assets/Scripts/Game Elements/InteractableObject.cs
@@ -1,18 +1,41 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using Sirenix.OdinInspector;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class InteractableObject : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+
+    public enum InteractionType
     {
-        
+        OnlyOnce,
+        Lever,
+        Button
     }
 
-    // Update is called once per frame
-    void Update()
+    public InteractionType type;
+    
+    [Title("Events")]
+    public UnityEvent OnInteractionStart = new UnityEvent();
+    public UnityEvent OnInteractionEnd = new UnityEvent();
+    
+    public void Interact()
     {
-        
+        switch (type)
+        {
+            case InteractionType.OnlyOnce:
+                
+                break;
+            case InteractionType.Lever:
+                
+                break;
+            case InteractionType.Button:
+                
+                break;
+            default:
+                throw new ArgumentOutOfRangeException();
+        }
     }
 }
