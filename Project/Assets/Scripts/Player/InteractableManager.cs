197e2482cb42501657e37fc10b2b8d9867a50cb5
Jordi Alba <44704875+jalba002@users.noreply.github.com>
2021-10-29 19:07:56 +0200

    Picking startup and edits

---
+++ Project/Assets/Scripts/Player/InteractableManager.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using Sirenix.OdinInspector;
+using UnityEngine;
+
+public class InteractableManager : MonoBehaviour
+{
+    public Collider pc;
+    public PickableObject lastPickable;
+    public Rigidbody connectedRB;
+    [Required] public BoxCollider attachedCollider;
+    private Vector3 halfSize;
+    
+    public T GetNearestItem<T>() where T : InteractableObject
+    {
+        Collider[] allColliders = new Collider[5];
+        Physics.OverlapBoxNonAlloc(transform.position, halfSize, allColliders);
+        foreach (Collider col in allColliders)
+        {
+            try
+            {
+                T item = col.gameObject.GetComponent<T>();
+                if (item != null) return item;
+            }
+            catch (NullReferenceException)
+            {
+            }
+        }
+
+        return null;
+    }
+}
