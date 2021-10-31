2d98adb2976b077d76cb80d30d9b810ce89c0da6
Andrew <73665985+andrewraya@users.noreply.github.com>
2021-10-31 02:36:32 +0100

    Level Design

---
+++ Project/Assets/ClipAndPlay.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClipAndPlay : MonoBehaviour
+{
+    public Animation Animation;
+
+    public void PlayClip(AnimationClip Clip)
+    {
+        Animation.clip = Clip;
+        Animation.Play();
+    }
+}
